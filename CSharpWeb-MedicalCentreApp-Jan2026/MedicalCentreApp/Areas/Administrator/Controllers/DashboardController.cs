3ec33da2db0ab0cbdebae035eff193567da9a460
ASYA <asyailieva9@gmail.com>
2026-04-01 14:31:25 +0100

    Create Admin Panel. Set up Index View

--- CSharpWeb-MedicalCentreApp-Jan2026/MedicalCentreApp/Areas/Administrator/Controllers/DashboardController.cs
+++ CSharpWeb-MedicalCentreApp-Jan2026/MedicalCentreApp/Areas/Administrator/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MedicalCentreApp.Services.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedicalCentreApp.Areas.Administrator.Controllers
@@ -7,8 +8,30 @@ namespace MedicalCentreApp.Areas.Administrator.Controllers
     [Authorize(Roles = "Administrator")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IDoctorService doctorService;
+        private readonly IPatientService patientService;
+        private readonly IAppointmentService appointmentService;
+        private readonly IDepartmentService departmentService;
+
+        public DashboardController(
+            IDoctorService doctorService,
+            IPatientService patientService,
+            IAppointmentService appointmentService,
+            IDepartmentService departmentService)
         {
+            this.doctorService = doctorService;
+            this.patientService = patientService;
+            this.appointmentService = appointmentService;
+            this.departmentService = departmentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.DoctorsCount = (await doctorService.GetAllAsync(null)).Count();
+            ViewBag.PatientsCount = (await patientService.GetAllAsync()).Count();
+            ViewBag.AppointmentsCount = (await appointmentService.GetAllAsync()).Count();
+            ViewBag.DepartmentsCount = (await departmentService.GetAllAsync()).Count();
+
             return View();
         }
     }
