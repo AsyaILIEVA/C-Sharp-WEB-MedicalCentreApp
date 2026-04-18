ef61fbff4d473fbcadf54a99c49420ae4e6f1b1c
ASYA <asyailieva9@gmail.com>
2026-04-18 18:14:16 +0100

    Move Admin logic from DoctorsController to Administrator Area - DoctorsManagementController

---
+++ CSharpWeb-MedicalCentreApp-Jan2026/MedicalCentreApp/Areas/Administrator/Controllers/DoctorsManagementController.cs
@@ -0,0 +1,145 @@
+﻿using MedicalCentreApp.Services.Core.Interfaces;
+using MedicalCentreApp.ViewModels.Doctors;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MedicalCentreApp.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    [Authorize(Roles = "Administrator")]
+    public class DoctorsManagementController : Controller
+    {
+        private readonly IDoctorService doctorService;
+        private readonly IDepartmentService departmentService;
+
+        public DoctorsManagementController(
+            IDoctorService doctorService,
+            IDepartmentService departmentService)
+        {
+            this.doctorService = doctorService;
+            this.departmentService = departmentService;
+        }
+                
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var departments = await departmentService.GetAllAsync();
+
+            var model = new CreateDoctorInputModel
+            {
+                Departments = departments
+                    .Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name
+                })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateDoctorInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                await PopulateDepartments(model);
+                return View(model);
+            }
+
+            try
+            {
+                await doctorService.CreateAsync(model);
+            }
+            catch (InvalidOperationException ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                await PopulateDepartments(model);
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Doctors", new { area = "" });
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await doctorService.GetForEditAsync(id);
+
+            if (model == null)
+                return NotFound();
+
+            await PopulateDepartments(model);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditDoctorInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                await PopulateDepartments(model);
+                return View(model);
+            }
+
+            var success = await doctorService.UpdateAsync(model);
+
+            if (!success)
+                return NotFound();
+
+            return RedirectToAction("Index", "Doctors", new { area = "" });
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var doctor = await doctorService.GetForDeleteAsync(id);
+
+            if (doctor == null)
+                return NotFound();
+
+            return View(doctor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var success = await doctorService.DeleteAsync(id);
+
+            if (!success)
+                return BadRequest("Doctor has appointments and cannot be deleted.");
+
+            return RedirectToAction("Index", "Doctors", new { area = "" });
+        }
+
+        private async Task PopulateDepartments(CreateDoctorInputModel model)
+        {
+            var departments = await departmentService.GetAllAsync();
+
+            model.Departments = departments
+                .Select(d => new SelectListItem
+            {
+                Value = d.Id.ToString(),
+                Text = d.Name
+            });
+        }
+
+        private async Task PopulateDepartments(EditDoctorInputModel model)
+        {
+            var departments = await departmentService.GetAllAsync();
+
+            model.Departments = departments
+                .Select(d => new SelectListItem
+            {
+                Value = d.Id.ToString(),
+                Text = d.Name,
+                Selected = d.Id == model.DepartmentId
+            });
+        }
+    }
+}
--- CSharpWeb-MedicalCentreApp-Jan2026/MedicalCentreApp/Controllers/DoctorsController.cs
+++ CSharpWeb-MedicalCentreApp-Jan2026/MedicalCentreApp/Controllers/DoctorsController.cs
@@ -1,9 +1,6 @@
-﻿using MedicalCentreApp.Services.Core;
-using MedicalCentreApp.Services.Core.Interfaces;
-using MedicalCentreApp.ViewModels.Doctors;
+﻿using MedicalCentreApp.Services.Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MedicalCentreApp.Controllers
 {    
@@ -18,7 +15,6 @@ namespace MedicalCentreApp.Controllers
             this.departmentService = departmentService;
         }
 
-
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Index(string? specialty, int pageNumber = 1, int pageSize = 10)
@@ -29,137 +25,6 @@ namespace MedicalCentreApp.Controllers
             ViewData["CurrentFilter"] = specialty;
 
             return View(pagedDoctors);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpGet]
-        public async Task<IActionResult> Create()
-        {
-            var departments = await departmentService.GetAllAsync();
-
-            var model = new CreateDoctorInputModel
-            {
-                Departments = departments.Select(d => new SelectListItem
-                {
-                    Value = d.Id.ToString(),
-                    Text = d.Name
-                })
-            };
-
-            return View(model);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(CreateDoctorInputModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                var departments = await departmentService.GetAllAsync();
-
-                model.Departments = departments.Select(d => new SelectListItem
-                {
-                    Value = d.Id.ToString(),
-                    Text = d.Name
-                });
-
-                return View(model);
-            }
-
-            try
-            {
-                await doctorService.CreateAsync(model);
-            }
-            catch (InvalidOperationException ex)
-            {
-                ModelState.AddModelError("", ex.Message);
-
-                var departments = await departmentService.GetAllAsync();
-                model.Departments = departments.Select(d => new SelectListItem
-                {
-                    Value = d.Id.ToString(),
-                    Text = d.Name
-                });
-
-                return View(model);
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpGet]        
-        public async Task<IActionResult> Edit(int id)
-        {
-            var model = await doctorService.GetForEditAsync(id);
-
-            if (model == null)
-                return NotFound();
-
-            var departments = await departmentService.GetAllAsync();
-
-            model.Departments = departments.Select(d => new SelectListItem
-            {
-                Value = d.Id.ToString(),
-                Text = d.Name,
-                Selected = d.Id == model.DepartmentId
-            });
-
-            return View(model);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(EditDoctorInputModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                var departments = await departmentService.GetAllAsync();
-
-                model.Departments = departments.Select(d => new SelectListItem
-                {
-                    Value = d.Id.ToString(),
-                    Text = d.Name,
-                    Selected = d.Id == model.DepartmentId
-                });
-
-                return View(model);
-            }
-
-
-            var success = await doctorService.UpdateAsync(model);
-
-            if (!success)
-                return NotFound();
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpGet]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var doctor = await doctorService.GetForDeleteAsync(id);
-
-            if (doctor == null)
-                return NotFound();
-
-            return View(doctor);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var success = await doctorService.DeleteAsync(id);
-
-            if (!success)
-                return BadRequest("Doctor has appointments and cannot be deleted.");
-
-            return RedirectToAction(nameof(Index));
         }
 
         [AllowAnonymous]
